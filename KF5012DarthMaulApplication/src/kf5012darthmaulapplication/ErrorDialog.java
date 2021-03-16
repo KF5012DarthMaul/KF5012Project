1219acef4fe6a21587a5a1b2d9fdd6a4c7bb9d38
Scrub <scott-donaldson@outlook.com>
2021-03-16 16:15:47 +0000

    Added GUI Error Dialog Class for Uniform Design

--- KF5012DarthMaulApplication/src/kf5012darthmaulapplication/ErrorDialog.java
+++ KF5012DarthMaulApplication/src/kf5012darthmaulapplication/ErrorDialog.java
@@ -1,5 +1,17 @@
 package kf5012darthmaulapplication;
 
-public class ErrorDialog {
+import javax.swing.JOptionPane;
 
+public class ErrorDialog {
+	public ErrorDialog(String errorText, Error error) {
+		this.generateJOptionPane(errorText, error);
+	}
+	
+	void generateJOptionPane(String errorText, Error error) {
+		String text = errorText;
+		String textError = error.getMessage();
+		String textStackTrace = error.getStackTrace().toString();
+		
+		JOptionPane.showMessageDialog(null, text + "\n" + textError + "\n" + textStackTrace,"An Error has occured", JOptionPane.ERROR_MESSAGE);
+	}
 }
