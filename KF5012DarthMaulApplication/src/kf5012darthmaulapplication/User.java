99f496db73690a999aa8247e2a00112722484f6a
Scrub <scott-donaldson@outlook.com>
2021-03-16 19:00:17 +0000

    Updated User class
    Added some basic error handling and comments

--- KF5012DarthMaulApplication/src/kf5012darthmaulapplication/User.java
+++ KF5012DarthMaulApplication/src/kf5012darthmaulapplication/User.java
@@ -2,22 +2,43 @@ package kf5012darthmaulapplication;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Scrub
+ *
+ */
+
 public class User {
 	String username;
 	int encodedPermission;
 	ArrayList<permissionsEnum> permissions = new ArrayList<>();
 	
+	/**
+	 * Permissions available to be given to a user object	
+	 */
 	enum permissionsEnum {
 		JANITOR,
 		HR,
 		SYSADMIN
 	}
-	
-	public User(String username, int permissionNumber) {
+	/**
+	 * Constructor for User, Takes permission number and gives user an ArrayList of Permissions from an Enum
+	 * @param username
+	 * @param permissionNumber
+	 */
+	public User(String username, int permissionNumber){
+		if(permissionNumber > (int) (Math.pow(2, permissionsEnum.values().length)) - 1) {
+			new ErrorDialog("Given Permission Value exceeds scope", new Error("Permission value too big: Got" + permissionNumber));
+			return;
+		}
+		
 		this.username = username;
 		generatePermissionList(permissionNumber);
 	}
-	
+	/**
+	 * Takes an integer number, converts it to a Binary string then the individual places of the binary number as boolean values to assign as a permission
+	 * @param permissionNumber
+	 */
 	private void generatePermissionList(int permissionNumber) {
 		char[] binArr = (Integer.toBinaryString(permissionNumber)).toCharArray();
 		for(int i = binArr.length; i >= 0; i--) {
@@ -26,6 +47,10 @@ public class User {
 			}else continue;
 		}
 	}
+	/**
+	 * Gets an ArrayList of permissions the User has
+	 * @return ArrayList<User.permissionsEnum>
+	 */
 	ArrayList<permissionsEnum> getPermissions(){
 		return permissions;
 	}
