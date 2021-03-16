89cf4827156298ceba09722c44ba940eda2a06e2
Scrub <scott-donaldson@outlook.com>
2021-03-16 17:39:39 +0000

    Created User Class
    Added Integer to Enum Permission function for bitwise permission generation

--- KF5012DarthMaulApplication/src/kf5012darthmaulapplication/User.java
+++ KF5012DarthMaulApplication/src/kf5012darthmaulapplication/User.java
@@ -1,5 +1,32 @@
 package kf5012darthmaulapplication;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	String username;
+	int encodedPermission;
+	ArrayList<permissionsEnum> permissions = new ArrayList<>();
+	
+	enum permissionsEnum {
+		JANITOR,
+		HR,
+		SYSADMIN
+	}
+	
+	public User(String username, int permissionNumber) {
+		this.username = username;
+		generatePermissionList(permissionNumber);
+	}
+	
+	private void generatePermissionList(int permissionNumber) {
+		char[] binArr = (Integer.toBinaryString(permissionNumber)).toCharArray();
+		for(int i = binArr.length; i >= 0; i--) {
+			if(binArr[i] == '1') {
+				permissions.add(permissionsEnum.values()[i]); 
+			}else continue;
+		}
+	}
+	ArrayList<permissionsEnum> getPermissions(){
+		return permissions;
+	}
 }
