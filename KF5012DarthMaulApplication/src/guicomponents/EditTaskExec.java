c1ce72f7b0e7908717125ead094f96798da03562
William Taylor <williamtaylordev@gmail.com>
2021-05-14 01:44:07 +0100

    Add verification execution editing to edit task execution gui

--- KF5012DarthMaulApplication/src/guicomponents/EditTaskExec.java
+++ KF5012DarthMaulApplication/src/guicomponents/EditTaskExec.java
@@ -1,14 +1,18 @@
 package guicomponents;
 
-import domain.TaskExecution;
 import domain.TaskPriority;
+import domain.TaskExecution;
+import domain.VerificationExecution;
+import guicomponents.ome.DomainObjectManager;
 import guicomponents.ome.ListSelectionEditor;
 import guicomponents.ome.LongTextEditor;
+import guicomponents.ome.VerificationExecutionEditor;
 import guicomponents.utils.ObjectEditor;
 
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +21,7 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 
 @SuppressWarnings("serial")
@@ -29,6 +34,10 @@ public class EditTaskExec
 	// Basic Fields
 	private LongTextEditor txteNotes;
 	private ListSelectionEditor<TaskPriority> lstePriority;
+	
+	// Verification Execution
+	private VerificationExecutionEditor edtVerificationExec;
+	private DomainObjectManager<VerificationExecution> omgVerificationExec;
 
 	/**
 	 * Create the panel.
@@ -39,9 +48,12 @@ public class EditTaskExec
 		GridBagLayout gbl_formPanel = new GridBagLayout();
 		gbl_formPanel.columnWidths = new int[]{0, 0, 0};
 		gbl_formPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_formPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_formPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_formPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_formPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		formPanel.setLayout(gbl_formPanel);
+
+		/* Basics
+		 * -------------------- */
 		
 		JLabel lblNotes = new JLabel("Notes");
 		GridBagConstraints gbc_lblNotes = new GridBagConstraints();
@@ -54,7 +66,7 @@ public class EditTaskExec
 		txteNotes = new LongTextEditor();
 		GridBagConstraints gbc_txteNotes = new GridBagConstraints();
 		gbc_txteNotes.insets = new Insets(5, 5, 5, 5);
-		gbc_txteNotes.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txteNotes.anchor = GridBagConstraints.WEST;
 		gbc_txteNotes.gridx = 1;
 		gbc_txteNotes.gridy = 0;
 		formPanel.add(txteNotes, gbc_txteNotes);
@@ -75,12 +87,12 @@ public class EditTaskExec
 		);
 		lstePriority.populate(Arrays.asList(TaskPriority.values()));
 		GridBagConstraints gbc_cmbPriority = new GridBagConstraints();
-		gbc_cmbPriority.fill = GridBagConstraints.HORIZONTAL;
+		gbc_cmbPriority.anchor = GridBagConstraints.WEST;
 		gbc_cmbPriority.insets = new Insets(5, 5, 5, 5);
 		gbc_cmbPriority.gridx = 1;
 		gbc_cmbPriority.gridy = 1;
 		formPanel.add(lstePriority, gbc_cmbPriority);
-		
+
 		JLabel lblNewLabel = new JLabel("<html><strong>Note:</strong> You can edit this task execution's allocation information (time and caretaker) in the \"Allocate Tasks\" tab on the left.</html>");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.insets = new Insets(10, 5, 10, 5);
@@ -88,6 +100,41 @@ public class EditTaskExec
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 2;
 		formPanel.add(lblNewLabel, gbc_lblNewLabel);
+
+		/* Verification Execution
+		 * -------------------- */
+
+		JSeparator sep1 = new JSeparator();
+		GridBagConstraints gbc_sep1 = new GridBagConstraints();
+		gbc_sep1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_sep1.insets = new Insets(0, 5, 5, 5);
+		gbc_sep1.gridwidth = 2;
+		gbc_sep1.gridx = 0;
+		gbc_sep1.gridy = 3;
+		formPanel.add(sep1, gbc_sep1);
+
+		edtVerificationExec = new VerificationExecutionEditor();
+		omgVerificationExec = new DomainObjectManager<>(
+			"Requires Verification", edtVerificationExec,
+			
+			// Create a new one each time the checkbox is re-ticked
+			() -> new VerificationExecution(
+				null, // No ID
+				active.getTask().getVerification(), // May be null
+				active,
+				"", // No notes
+				Duration.ofMinutes(0), // Zero deadline (user should set it)
+				null, // No allocation
+				null // Not completed
+			)
+		);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(5, 5, 5, 5);
+		gbc_panel.anchor = GridBagConstraints.WEST;
+		gbc_panel.gridwidth = 2;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 4;
+		formPanel.add(omgVerificationExec, gbc_panel);
 	}
 
 	@Override
@@ -108,6 +155,7 @@ public class EditTaskExec
 		
 		txteNotes.setObject(taskExec.getNotes());
 		lstePriority.setObject(taskExec.getPriority());
+		omgVerificationExec.getObjectManager().setObject(taskExec.getVerification());
 	}
 
 	/**
@@ -121,6 +169,7 @@ public class EditTaskExec
 
 		if (!txteNotes.validateFields()) valid = false;
 		if (!lstePriority.validateFields()) valid = false;
+		if (!edtVerificationExec.validateFields()) valid = false;
 		
 		return valid;
 	}
@@ -135,6 +184,7 @@ public class EditTaskExec
 	public TaskExecution getObject() {
 		active.setNotes(txteNotes.getObject());
 		active.setPriority(lstePriority.getObject());
+		active.setVerification(omgVerificationExec.getObjectManager().getObject());
 		
 		return active;
 	}
