7a7036714a796de99f9ab3520e5da7fbd3d97bc7
William Taylor <williamtaylordev@gmail.com>
2021-05-15 17:02:49 +0100

    General allocation GUI

--- KF5012DarthMaulApplication/src/guicomponents/ManageAllocation.java
+++ KF5012DarthMaulApplication/src/guicomponents/ManageAllocation.java
@@ -1,14 +1,30 @@
 package guicomponents;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
 
+@SuppressWarnings("serial")
 public class ManageAllocation extends JPanel {
-
 	/**
 	 * Create the panel.
 	 */
 	public ManageAllocation() {
-
+		setLayout(new BorderLayout(0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel generationPanel = new JPanel();
+		tabbedPane.addTab("Task Generation", null, generationPanel, null);
+		generationPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel allocationPanel = new JPanel();
+		tabbedPane.addTab("Task Allocation", null, allocationPanel, null);
 	}
-
 }
