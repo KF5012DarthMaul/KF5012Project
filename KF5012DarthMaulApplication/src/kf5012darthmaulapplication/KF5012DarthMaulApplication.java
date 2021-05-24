102a9775e56e466279f49550f55af0148232ca09
Emanuel <emanuelfpoliveira@gmail.com>
2021-05-24 03:32:20 +0100

    Added comments to initialiser

--- KF5012DarthMaulApplication/src/kf5012darthmaulapplication/KF5012DarthMaulApplication.java
+++ KF5012DarthMaulApplication/src/kf5012darthmaulapplication/KF5012DarthMaulApplication.java
@@ -12,25 +12,30 @@ public class KF5012DarthMaulApplication {
 		try {
 			//Set the keysize to unlimited (MAXINT)
 			Security.setProperty("crypto.policy", "unlimited");
-                        // Init the DB
+                        
 			DBAbstraction db = DBAbstraction.getInstance();
+                        // Default user
                         User inituser = new User("default", "Default Initial User", AccountType.INITUSER);
                         var userList = db.getAllUsers();
-                        if(userList.isEmpty())
+                        boolean hasHR = !userList.stream().filter(u -> u.accountType == AccountType.HR_PERSONNEL).collect(Collectors.toList()).isEmpty();
+                        // Check if there are any users in the system, if there are none, that could mean the DB is uninitialized
+                        if(userList.isEmpty()) 
                         {
+                            // Initiliaze the DB
                             InitialiseDB initdb = new InitialiseDB();
                             initdb.dropTables();
                             initdb.createTables();
                         }
-                        boolean hasHR = !userList.stream().filter(u -> u.accountType == AccountType.HR_PERSONNEL).collect(Collectors.toList()).isEmpty();
+                        // Check if there are any HR users registered in the DB
                         if(!hasHR)
                         {
+                            // If there aren't, create the default user if it doesnt exist already
                             if(!db.doesUserExist("default"))
                                 db.createUser(inituser, SecurityManager.generatePassword("northumbria"));
-                            else
+                            else // If it exists, reset its password to northumbria
                                 db.setHashedPassword(db.getUser("default"), SecurityManager.generatePassword("northumbria"));
                         }
-                        else if(db.doesUserExist("default"))
+                        else if(db.doesUserExist("default")) // if there are HR users, delete the default user if it exists
                         {
                             db.deleteUser(inituser);
                         }
